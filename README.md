41182171b8f28fa76f492a83c6636948c524f1c7
Gibsol <gibsol@tutanota.com>
2022-01-13 15:58:06 +0200

    added a variable that shows, how many minutes you have worked

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # Terminal Pomodoro
 
-A basic terminal pomodoro application written in C++ (Only for Linux) 
+A basic terminal pomodoro application written in C++ (Only for Linux). 
 
 ## Installation
 1. Clone the repo.
-2. Compile the source code.
-3. Launch the program.
+2. Launch the a.out file.  
 
 ## License
-GNU General Public License v3.0
+GNU General Public License v3.0.
 
 ## Project status
-In development
+In development.
--- main.cpp
+++ main.cpp
@@ -9,14 +9,16 @@ int work_minutes;
 int rest_minutes;
 int work;
 int rest;
+int total_time;  // total working time
 char user_exit;
-
 
 // creates the choice for a user of how many minutes would they want to work and rest.
 void user_count(){ 
   std::cout << "enter how many minutes would you want to work: ";
   std::cin >> work;
   
+  total_time += work;
+
   std::cout << "enter how many minutes would you want to rest: ";
   std::cin >> rest;
 }
@@ -79,9 +81,14 @@ int main(){
     
     pomodoro_work();
     pomodoro_rest();
-    
+   
+    work = 0;
+    rest = 0;
+
+
     system("clear"); 
-    
+  
+    std::cout << "you have been working for " << total_time << " minute(s)" << std::endl;
     std::cout << "would you want to continue? Y/n" << std::endl;
     std::cin >> user_exit;
   }
